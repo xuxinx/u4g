f93b152ba881c8046cbb532030d1d330b6dbb9f7
xuxin <xuxinmight@gmail.com>
2019-12-17 14:55:47 +0800

    Add gorm transaction util

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/xuxinx/u4g
 
 go 1.13
 
-require github.com/stretchr/testify v1.4.0
+require (
+	github.com/jinzhu/gorm v1.9.11
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/stretchr/testify v1.4.0
+)
---
+++ u4gorm/tx.go
@@ -0,0 +1,32 @@
+package u4gorm
+
+import (
+	"fmt"
+	"log"
+	"runtime/debug"
+
+	"github.com/jinzhu/gorm"
+)
+
+func Transact(db *gorm.DB, f func(tx *gorm.DB) error) (err error) {
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			if aerr, ok := r.(error); ok {
+				err = aerr
+			} else {
+				err = fmt.Errorf("%+v", r)
+			}
+			log.Printf("transact recover error: %v", err)
+			debug.PrintStack()
+		}
+
+		if err != nil {
+			err = tx.Rollback().Error
+		} else {
+			err = tx.Commit().Error
+		}
+	}()
+
+	return f(tx)
+}
